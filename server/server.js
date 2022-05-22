f607d0ab9ad0006beb88625c1138f875a8b69296
Forever Young <dy9040@gmail.com>
2022-05-22 17:34:20 -0500

    add more files.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -10,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers
+  // context: authMiddleware
 });
 
 const app = express();
