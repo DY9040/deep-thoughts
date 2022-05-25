7508c66cd4a00e043e20fc3a10bae5f9199d170a
Forever Young <dy9040@gmail.com>
2022-05-24 20:16:50 -0500

    Client-side login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,12 @@ import {
   createHttpLink,
 } from '@apollo/client';
 
+import { setContext } from '@apollo/client/link/context';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
-
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
@@ -22,8 +23,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link:  authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -47,9 +58,13 @@ function App() {
                 path="/signup"
                 element={<Signup />}
               />
-              <Route
-                path="/profile"
+               <Route
+                path="/profile/:username"
                 element={<Profile />}
+              />
+               <Route 
+                path="/profile" 
+                element={<Profile />} 
               />
               <Route
                 path="/thought/:id"
