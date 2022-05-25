b7d6782866da239f6295d56e5424bbda759acbec
Forever Young <dy9040@gmail.com>
2022-05-25 08:36:48 -0500

    fixed componet files

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -8,6 +8,7 @@ const Header = () => {
     event.preventDefault();
     Auth.logout();
   };
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -16,7 +17,7 @@ const Header = () => {
         </Link>
 
         <nav className="text-center">
-          {Auth.loggedIn () ? (
+          {Auth.loggedIn() ? (
             <>
               <Link to="/profile">Me</Link>
               <a href="/" onClick={logout}>
--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -4,44 +4,47 @@ import { useMutation } from '@apollo/client';
 import { ADD_REACTION } from '../../utils/mutations';
 
 const ReactionForm = ({ thoughtId }) => {
-const [reactionBody, setBody] = useState('');
-const [characterCount, setCharacterCount] = useState(0);
-const [addReaction, { error }] = useMutation(ADD_REACTION);
+  const [reactionBody, setBody] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+  const [addReaction, { error }] = useMutation(ADD_REACTION);
 
+  // update state based on form input changes
+  const handleChange = (event) => {
+    if (event.target.value.length <= 280) {
+      setBody(event.target.value);
+      setCharacterCount(event.target.value.length);
+    }
+  };
 
-const handleChange = event => {
-  if (event.target.value.length <= 280) {
-    setBody(event.target.value);
-    setCharacterCount(event.target.value.length);
-  }
-};
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-const handleFormSubmit = async event => {
-  event.preventDefault();
+    try {
+      await addReaction({
+        variables: { reactionBody, thoughtId },
+      });
 
-  try {
-    await addReaction({
-      variables: { reactionBody, thoughtId },
-    });
+      // clear form value
+      setBody('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-    // clear form value
-    setBody('');
-    setCharacterCount(0);
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-return (
+  return (
     <div>
-      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
+      <p
+        className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
       >
         Character Count: {characterCount}/280
-        {error && <span className="ml-2">Something went wrong</span>}
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
-      <form className="flex-row justify-center justify-space-between-md align-stretch"
-          onSubmit={handleFormSubmit}
-          >
+      <form
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleFormSubmit}
+      >
         <textarea
           placeholder="Leave a reaction to this thought..."
           value={reactionBody}
